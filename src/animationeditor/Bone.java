ce33cb32cb48442c1a9e6096645a1fff3f0d8d19
Corey Young <corey_young@outlook.com>
2014-04-08 20:20:01 +1000

    reinitialized files

---
+++ src/animationeditor/Bone.java
@@ -0,0 +1,84 @@
+package animationeditor;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import javax.swing.tree.DefaultMutableTreeNode;
+
+public class Bone {
+
+    String name;
+    int x, y, dir, length;
+    ArrayList<Bone> childList = new ArrayList<>();
+
+    public Bone(String name, int x, int y, int dir, int length) {
+        this.x = x;
+        this.y = y;
+        this.dir = dir;
+        this.length = length;
+        this.name = name;
+
+        Skeleton.totalBones++;
+    }
+
+    public Bone getDescendant(String name) {
+        for (Bone child : childList) {
+            if (child.name.equals(name)) {
+                return child;
+            } else {
+                Bone grandChild = child.getDescendant(name);
+                
+                if (grandChild != null) {
+                    return grandChild;
+                }
+            }
+        }
+        
+        return null;
+    }
+    
+    public void removeDescendant (String name) {
+        Iterator childIterator = childList.iterator();
+        
+        while (childIterator.hasNext()) {
+            Bone child = (Bone) childIterator.next();
+            
+            if (child.name.equals(name)) {
+                childIterator.remove();
+            } else {
+                child.removeDescendant(name);
+            }
+        }
+    }
+    
+    public DefaultMutableTreeNode getTreeBranch() {
+        DefaultMutableTreeNode node = new DefaultMutableTreeNode(name);
+        
+        for (Bone child : childList) {
+            DefaultMutableTreeNode childNode = child.getTreeBranch();
+            node.add(childNode);
+        }
+        
+        return node;
+    }
+    
+    public HashMap<String, Object> getBoneMap() {
+        HashMap<String, Object> boneMap = new HashMap<>();
+        
+        boneMap.put("Name", name);
+        boneMap.put("X", x);
+        boneMap.put("Y", x);
+        boneMap.put("Direction", x);
+        boneMap.put("Length", x);
+        
+        ArrayList<HashMap> childMap = new ArrayList<>();
+        
+        for (Bone child : childList) {
+            childMap.add(child.getBoneMap());
+        }
+        
+        boneMap.put("Child Map", childMap);
+        
+        return boneMap;
+    }
+}
---
+++ src/animationeditor/Skeleton.java
@@ -0,0 +1,84 @@
+package animationeditor;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+
+public abstract class Skeleton {
+
+    public static int totalBones = 0;
+    public static int x = 64;
+    public static int y = 32;
+    public static ArrayList<Bone> boneList = new ArrayList<>();
+    
+    public static void removeBone(String name) {
+        Iterator iterator = boneList.iterator();
+        
+        while (iterator.hasNext()) {
+            Bone bone = (Bone) iterator.next();
+
+            if (bone.name.equals(name)) {
+                iterator.remove();
+            } else {
+                bone.removeDescendant(name);
+            }
+        }
+    }
+    
+    public static Bone getBone(String name) {
+        for (Bone bone : boneList) {
+            if (bone.name.equals(name)) {
+                return bone;
+            } else {
+                Bone descendant = bone.getDescendant(name);
+                
+                if (descendant != null) {
+                    return descendant;
+                }
+            }
+        }
+        
+        return null;
+    }
+    
+    public static ArrayList<HashMap> getBoneList() {
+        ArrayList<HashMap> mapList = new ArrayList<>();
+        
+        for (Bone bone : boneList) {
+            mapList.add(bone.getBoneMap());
+        }
+        
+        return mapList;
+    }
+    
+    public static void setBoneList(ArrayList<HashMap> fileMap) {
+        boneList.clear();
+        
+        for (HashMap boneMap : fileMap) {
+            boneList.add(loadBoneFromMap(boneMap));
+        }
+    }
+    
+    private static Bone loadBoneFromMap(HashMap<String, Object> boneMap) {
+        String name = (String) boneMap.get("Name");
+        int x = (int) boneMap.get("X");
+        int y = (int) boneMap.get("Y");
+        int dir = (int) boneMap.get("Direction");
+        int length = (int) boneMap.get("Length");
+        
+        Bone bone = new Bone(name, x, y, dir, length);
+        
+        ArrayList<HashMap> childMapList = (ArrayList<HashMap>) boneMap.get("Child Map");
+        ArrayList<Bone> childList = new ArrayList<>();
+        
+        for (HashMap<String, Object> childMap : childMapList) {
+            if (! childMap.isEmpty()) {
+                childList.add(loadBoneFromMap(childMap));
+            }
+        }
+            
+        bone.childList = childList;
+        
+        return bone;
+    }
+}
