7533eabb6eba38f8a1536a12b8d15898d24ef6db
Corey Young <corey_young@outlook.com>
2015-05-29 16:23:08 +1000

    Cleaned up unit tests.

--- test/animationeditor/AnimationEditorGUITest.java
+++ test/animationeditor/AnimationEditorGUITest.java
@@ -1,52 +1,16 @@
 package animationeditor;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 public class AnimationEditorGUITest {
-    
-    public AnimationEditorGUITest() {
-    }
 
-    @BeforeClass
-    public static void setUpClass() throws Exception {
-    }
-
-    @AfterClass
-    public static void tearDownClass() throws Exception {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
-
-    /**
-     * Test of main method, of class AnimationEditorGUI.
-     */
-    @Test
-    public void testMain() {
-        System.out.println("main");
-        String[] args = null;
-        AnimationEditorGUI.main(args);
-    }
-
-    /**
-     * Test of redrawSkeleton method, of class AnimationEditorGUI.
-     */
-    @Test
-    public void testRedrawSkeleton() {
-        System.out.println("redrawSkeleton");
-        String[] args = null;
-        AnimationEditorGUI.main(args);
-        AnimationEditorGUI.skeletonPanel = new SkeletonPanel();
-        AnimationEditorGUI.redrawSkeleton();
-    }
+	/**
+	 * Test of main method, of class AnimationEditorGUI.
+	 */
+	@Test
+	public void testMain() {
+		System.out.println("main");
+		String[] args = null;
+		AnimationEditorGUI.main(args);
+	}
 }
--- test/animationeditor/AnimationTest.java
+++ test/animationeditor/AnimationTest.java
@@ -1,64 +1,41 @@
 package animationeditor;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class AnimationTest {
-    
-    public AnimationTest() {
-    }
 
-    @BeforeClass
-    public static void setUpClass() throws Exception {
-    }
+	/**
+	 * Test of addKeyFrame method, of class Animation.
+	 */
+	@Test
+	public void testAddKeyFrame() {
+		System.out.println("addKeyFrame");
 
-    @AfterClass
-    public static void tearDownClass() throws Exception {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
+		Animation.keyFrameMap.clear();
 
-    /**
-     * Test of addKeyFrame method, of class Animation.
-     */
-    @Test
-    public void testAddKeyFrame() {
-        System.out.println("addKeyFrame");
-        
-        Animation.keyFrameMap.clear();
-        
-        int time = 0;
-        Animation.addKeyFrame(time);
-        
-        assert(Animation.keyFrameMap.containsKey(time));
-    }
+		int time = 0;
+		Animation.addKeyFrame(time);
 
-    /**
-     * Test of getKeyFrame method, of class Animation.
-     */
-    @Test
-    public void testGetKeyFrame() {
-        System.out.println("getKeyFrame");
+		assert (Animation.keyFrameMap.containsKey(time));
+	}
 
-        Animation.keyFrameMap.clear();
-        
-        int time = 0;
-        KeyFrame expResult = null;
-        KeyFrame result = Animation.getKeyFrame(time);
-        assertEquals(expResult, result);
-        
-        Animation.addKeyFrame(time);
-        result = Animation.getKeyFrame(time);
-        assertEquals(result.getTime(), time);
-    }
+	/**
+	 * Test of getKeyFrame method, of class Animation.
+	 */
+	@Test
+	public void testGetKeyFrame() {
+		System.out.println("getKeyFrame");
+
+		Animation.keyFrameMap.clear();
+
+		int time = 0;
+		KeyFrame expResult = null;
+		KeyFrame result = Animation.getKeyFrame(time);
+		assertEquals(expResult, result);
+
+		Animation.addKeyFrame(time);
+		result = Animation.getKeyFrame(time);
+		assertEquals(result.getTime(), time);
+	}
 }
